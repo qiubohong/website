69845fb833e1d60a503d51609716a2bda941ebf2
borfyqiu <borfyqiu@tencent.com>
2023-05-25 11:42:39 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
+source ~/.bash_profile
 nvm use v14.21.3
 yarn build
 git add . *
 git commit -m "update"
 git push
 
-rsync -av -e ssh  /Users/borfyqiu/Desktop/study/self/website/dist/* root@1.13.188.124:/data/borfyqiu.oa.com/
+rsync -av -e ssh  //Users/borfyqiu/Desktop/study/github/website/dist/* root@1.13.188.124:/data/borfyqiu.oa.com/
