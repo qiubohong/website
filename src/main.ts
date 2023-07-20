9320abd95aa7cb0f21a9a316f964b6b62581e572
borfyqiu <borfyqiu@tencent.com>
2023-07-20 19:25:22 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // 引入组件库的少量全局样式变量
-import 'tdesign-vue-next/es/style/index.css';
+// import 'tdesign-vue-next/es/style/index.css';
 const app = createApp(App)
 // app.use(TDesign) 使用按需引入
 
